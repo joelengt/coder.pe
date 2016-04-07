423954309d8d1eec269d6cd110140263261d6cd4
Joel Gonzales Tipismana <joelengt@gmail.com>
2016-04-07 13:37:54 -0500

    fondo cambiado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-window.addEventListener('load', function() {
-	setTimeout(function(){
-		window.location.replace('https://www.facebook.com/CoderAgenciaDigital')
-	}, 1000)
-})
+// window.addEventListener('load', function() {
+// 	setTimeout(function(){
+// 		window.location.replace('https://www.facebook.com/CoderAgenciaDigital')
+// 	}, 1000)
+// })
